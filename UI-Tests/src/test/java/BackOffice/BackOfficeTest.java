fe23fb3c89fa562c0426e9f19b8d2d5d67b48cd6
Vika Manaicheva <manaichevavika@gmail.com>
2025-06-15 10:00:15 +0300

    test: added delete language test, manage events tests, manage sports tests and searching sport string test

--- UI-Tests/src/test/java/BackOffice/BackOfficeTest.java
+++ UI-Tests/src/test/java/BackOffice/BackOfficeTest.java
@@ -2,13 +2,14 @@ package BackOffice;
 
 import LoadConfig.ConfigLoader;
 import LogIn.LogInPage;
+
 import io.qameta.allure.Step;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.TimeoutException;
-import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -17,8 +18,8 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class BackOfficeTest {
     private WebDriver driver;
@@ -30,7 +31,6 @@ public class BackOfficeTest {
     @BeforeEach
     @Step("")
     void setUp() {
-        //Для отладки - из-за нестандартного расширения экрана
         ChromeOptions options = new ChromeOptions();
         options.addArguments("force-device-scale-factor=1");
 
@@ -42,11 +42,7 @@ public class BackOfficeTest {
 
         logInPage = new LogInPage(driver);
         backOfficePage = new BackOfficePage(driver, wait);
-    }
 
-    @BeforeEach
-    @Step("")
-    void startSettings() {
         logInSystem();
         goToConfigQA();
     }
@@ -60,26 +56,103 @@ public class BackOfficeTest {
     @Test
     @DisplayName("")
     void testAddLanguage() {
-        backOfficePage.clickEditLanguageBtn();
-        backOfficePage.clickAddLanguageBtn();
-        backOfficePage.chooseAfrikaansLanguage();
-        backOfficePage.clickAddBtnOfModalWindow();
-        backOfficePage.clickSaveLanguageSettingsBtn();
-        backOfficePage.clickSaveConfigBtn();
+        addTestLanguage();
 
         assertThat("",
                 isSuccessMessageDisplayed(), is(true));
 
         assertThat("",
-                isAfrikaansLanguageDisplayed(), is(true));
+                isLanguageDisplayed(), is(true));
     }
 
-//    @Test
-//    @DisplayName("")
-//    void testAddLanguageWithInvalidCount() {
-//    }
+    @Test
+    @DisplayName("")
+    void testDeleteLanguage() {
+        addTestLanguage();
+        deleteTestLanguage();
 
+        assertThat("",
+                isSuccessMessageDisplayed(), is(true));
 
+        assertThat("",
+                isLanguageDisplayed(), is(false));
+    }
+
+    @Test
+    @DisplayName("")
+    void testAddEventUsingHighlightManager() {
+        addTestLanguage();
+        addTestSport();
+
+        addTestSportEvent();
+        backOfficePage.clickSaveConfigBtn();
+
+        assertThat("",
+                isSportEventDisplayed(), is(true));
+    }
+
+    @Test
+    @DisplayName("")
+    void testAddEventUsingCopyEventsBtn() {
+        addTestSport();
+        addTestSportEventToDefault();
+        addTestLanguage();
+
+        backOfficePage.clickCopyEventsBtn();
+        backOfficePage.clickSaveConfigBtn();
+
+        assertThat("",
+                isSportEventDisplayed(), is(true));
+    }
+
+    @Test
+    @DisplayName("")
+    void testDeleteEventUsingHighlightsManager() {
+        addTestLanguage();
+        addTestSport();
+        addTestSportEvent();
+
+        backOfficePage.clickDeleteEventBtn();
+        backOfficePage.clickSaveConfigBtn();
+
+        assertThat("",
+                isSportEventNotDisplayed(), is(true));
+    }
+
+    @Test
+    @DisplayName("")
+    void testAddSport() {
+        addTestSport();
+        backOfficePage.clickSaveConfigBtn();
+
+        assertThat("",
+                isSportDisplayed(), is(true));
+    }
+
+    @Test
+    @DisplayName("")
+    void testDeleteSport() {
+        addTestSport();
+
+        backOfficePage.clickSportCheckbox();
+        backOfficePage.clickDeleteSportBtn();
+        backOfficePage.clickSaveConfigBtn();
+
+        assertThat("",
+                isSportDisplayed(), is(false));
+    }
+
+    @Test
+    @DisplayName("")
+    void testAddSportSearchingString() throws InterruptedException {
+        backOfficePage.clickEditLanguageBtn();
+        backOfficePage.clickAddLanguageBtn();
+        backOfficePage.enterTextIntoSearchLanguageString("norwegian");
+        backOfficePage.clickSearchLanguageBtn();
+
+        assertThat("",
+                backOfficePage.getNameOfSearchLanguage(), equalTo("Norwegian"));
+    }
 
 
     @Step("")
@@ -101,10 +174,54 @@ public class BackOfficeTest {
     }
 
     @Step("")
-    private boolean isAfrikaansLanguageDisplayed() {
+    private void addTestLanguage() {
+        backOfficePage.clickEditLanguageBtn();
+        backOfficePage.clickAddLanguageBtn();
+        backOfficePage.chooseLanguage();
+        backOfficePage.clickAddBtnOfModalWindow();
+        backOfficePage.clickSaveLanguageSettingsBtn();
+        backOfficePage.clickSaveConfigBtn();
+        backOfficePage.clickLanguageBtn();
+    }
+
+    @Step("")
+    private void deleteTestLanguage() {
+        backOfficePage.clickEditLanguageBtn();
+        backOfficePage.clickDeleteLanguageBtn();
+        backOfficePage.chooseLanguage();
+        backOfficePage.clickAddBtnOfModalWindow();
+        backOfficePage.clickSaveLanguageSettingsBtn();
+        backOfficePage.clickSaveConfigBtn();
+    }
+
+    @Step("")
+    private void addTestSport() {
+        backOfficePage.clickAddSportMenu();
+        backOfficePage.chooseSport();
+        backOfficePage.clickAddSportMenuApplyBtn();
+    }
+
+
+
+    @Step("")
+    private void addTestSportEvent() {
+        backOfficePage.clickSport();
+        backOfficePage.clickSportCountry();
+        backOfficePage.clickSportChampionship();
+        backOfficePage.clickAddSportEvent();
+    }
+
+    @Step("")
+    private void addTestSportEventToDefault() {
+        backOfficePage.clickDefaultBtn();
+        addTestSportEvent();
+    }
+
+    @Step("")
+    private boolean isLanguageDisplayed() {
         try {
             return wait
-                    .until(ExpectedConditions.visibilityOfElementLocated(backOfficePage.getAfrikaansLanguageBtn()))
+                    .until(ExpectedConditions.visibilityOfElementLocated(backOfficePage.getLanguageBtn()))
                     .isDisplayed();
         } catch (TimeoutException e) {
             return false;
@@ -117,6 +234,37 @@ public class BackOfficeTest {
             return wait
                     .until(ExpectedConditions.visibilityOfElementLocated(backOfficePage.getSuccessMessage()))
                     .isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    @Step("")
+    private boolean isSportDisplayed() {
+        try {
+            return wait
+                    .until(ExpectedConditions.visibilityOfElementLocated(backOfficePage.getSport()))
+                    .isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    @Step("")
+    private boolean isSportEventDisplayed() {
+        try {
+            return wait
+                    .until(ExpectedConditions.visibilityOfElementLocated(backOfficePage.getSportEvent()))
+                    .isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    @Step("")
+    private boolean isSportEventNotDisplayed() {
+        try {
+            return wait.until(ExpectedConditions.invisibilityOfElementLocated(backOfficePage.getSportEvent()));
         } catch (TimeoutException e) {
             return false;
         }
